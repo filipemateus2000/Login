b978c60ea6810039a0568cc243aca070f8300166
filipemateus2000 <filipemateus2000@hotmail.com>
2023-09-07 23:25:54 -0300

    Validação de login e senha criados

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+    }
+}
